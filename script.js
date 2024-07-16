f7997de1a6c4fda74665728840b8b80c906ecd8f
Anshuman Khanna <127147765+Anshumankhanna@users.noreply.github.com>
2024-07-16 20:13:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+let contactUs = document.querySelector("#contact-us");
+let form = document.querySelector("form");
+let flipImage = document.querySelectorAll(".flip-image");
+let dots = document.querySelectorAll(".float > *");
+let options = document.querySelectorAll(".options > div");
+let sectionImage = document.querySelector(".section .image");
+let sectionImageArr = [`images/section-image1.jpg`, `images/section-image2.png`, `images/section-image3.webp`];
+
+contactUs.addEventListener("click", () => {
+    form.style.scale = "1.1";
+    form.style.opacity = "1";
+
+    setTimeout(() => {
+        form.style.scale = "1";
+    }, 400);
+});
+
+document.addEventListener("click", (dets) => {
+    if (dets.target !== contactUs && dets.target !== form) {
+        form.style.scale = "1.1";
+
+        setTimeout(() => {
+            form.style.scale = "0";
+            form.style.opacity = "0";
+        }, 400);
+    }
+});
+
+function highlight(special) {
+    dots.forEach(val => {
+        if (val.id === special) {
+            val.childNodes[1].style.opacity = "1";
+            val.childNodes[3].style.opacity = "0";
+        } else {
+            val.childNodes[3].style.opacity = "1";
+            val.childNodes[1].style.opacity = "0";
+        }
+    });
+};
+
+highlight("dot1");
+
+flipImage.forEach((val, index) => {
+    val.addEventListener("mouseenter", () => {
+        val.style.rotate = "y 180deg";
+        val.childNodes[1].style.opacity = "0";
+        val.childNodes[3].style.opacity = "1";
+        val.childNodes[3].style.transitionDelay = "0.2s";
+        
+        highlight(`dot${index + 1}`)
+    });
+    
+    val.addEventListener("mouseleave", () => {
+        val.style.rotate = "y 0deg";
+        val.childNodes[1].style.opacity = "1";
+        val.childNodes[3].style.opacity = "0";
+        val.childNodes[1].style.transitionDelay = "0.2s";
+
+        highlight("dot1");
+    });
+});
+
+options.forEach((val, index) => {
+    val.addEventListener("mouseenter", () => {
+        val.style.backgroundColor = "#ff3147";
+    });
+    val.addEventListener("click", () => {
+        sectionImage.style.backgroundImage = `url(${sectionImageArr[index]})`;
+    });
+    val.addEventListener("mouseleave", () => {
+        val.style.backgroundColor = "transparent";
+    });
+});
